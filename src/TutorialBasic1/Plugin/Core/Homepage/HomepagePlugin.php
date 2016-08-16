5270036b2fe833506b031f17e61fdf2619e84d24
matthes <leuffen@continue.de>
2016-08-16 18:46:01 +0200

    Initial

--- src/TutorialBasic1/Plugin/Core/Homepage/HomepagePlugin.php
+++ src/TutorialBasic1/Plugin/Core/Homepage/HomepagePlugin.php
@@ -10,6 +10,8 @@
 
 
     use Gismo\Component\Application\Context;
+    use Gismo\Component\Di\DiCallChain;
+    use Gismo\Component\HttpFoundation\Request\Request;
     use Gismo\Component\Plugin\Plugin;
     use Gismo\TutorialBasic1\Context\Homepage\HomepageContext;
 
@@ -20,6 +22,43 @@
             if ($context instanceof HomepageContext) {
 
                 $context["page.mainLayout"][0] = $context->templateFile(__DIR__ . "/Tpl/MainLayout.tpl.html");
+
+
+                /* Don't do that:
+                $context["api.product.get"][0] = function ($id) {
+                    return ["Some" => "data for $id"];
+                };
+
+
+                $context["api.product.get"][-1] = function ($§§return) {
+                    $§§return["otherData"] =  "Filtermich";
+                    return $§§return;
+                };
+                * Always use filter():
+                */
+
+                $context["api.product.get"] = $context->filter(function (DiCallChain $§§input) {
+                    $§§input[0] = function ($id) {
+                        return ["Some" => "data for $id"];
+                    };
+                });
+
+                $context["api.product.get"] = $context->filter(function (DiCallChain $§§input) {
+                    $§§input[-1] = function ($§§return, $id) {
+                        $§§return["Some other"] = "data for $id";
+                        return $§§return;
+                    };
+                });
+
+
+                $context->route->add("GET@/api/product/:id", function ($§§parameters, Request $request) use ($context) {
+
+
+                    $ret = $context["api.product.get"]($§§parameters);
+                    echo json_encode($ret);
+
+                });
+
 
                 $context->route->add("/", function () use ($context) {
                     echo $context["page.mainLayout"](["title" => "Ein toller Titel"]);
